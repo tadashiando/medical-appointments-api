b69fd32ca202307fccf0056059e519133cff0fbf
Tadashi Ando <ando.tadashi@gmail.com>
2025-05-28 18:37:34 -0300

    test: setup Jest configuration and test dependencies
    - Add Jest with TypeScript support (ts-jest preset)
    - Configure test environment and coverage settings
    - Add test scripts to package.json (test, test:watch, test:coverage)
    - Configure test file patterns and setup files
    
    Dependencies added:
    - jest, @types/jest, ts-jest, supertest, @types/supertest

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  roots: ["<rootDir>/src"],
+  testMatch: ["**/__tests__/**/*.test.ts"],
+  collectCoverageFrom: [
+    "src/**/*.ts",
+    "!src/**/*.d.ts",
+    "!src/server.ts",
+    "!src/types/**",
+    "!src/__tests__/**",
+  ],
+  coverageDirectory: "coverage",
+  coverageReporters: ["text", "lcov", "html"],
+  setupFilesAfterEnv: ["<rootDir>/src/__tests__/setup.ts"],
+  verbose: true,
+  testTimeout: 10000,
+};
